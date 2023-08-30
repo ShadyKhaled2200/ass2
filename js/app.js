e31c7e711dc02e325550ba1920e8a79d3d8ff007
shady <shadykhaled1235@gmail.com>
2023-08-30 05:30:23 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -1,101 +1,92 @@
+$('.open').click(function(){
+    $('nav').css('left', '0')
+})
+$('.fa-xmark').click(function(){
+    $('nav').css('left', '-240px')
+})
 
-var quotes=[
-    {
-        text:"''It is our choices, Harry, that show what we truly are, far more than our abilities.''",
-        author:"-- J.K. Rowling",
-    },
-    {
-        text:"''A friend is someone who knows all about you and still loves you.''",
-        author:"-- Elbert Hubbard",
-    },
-    {
-        text:"''If you can't explain it to a six year old, you don't understand it yourself.''",
-        author:" -- Albert Einstein",
-    },
-    {
-        text:"''Everything you can imagine is real.''",
-        author:" -- Pablo Picasso",
-    },
-    {
-        text:"''We don't see things as they are, we see them as we are.''",
-        author:" -- Anais Nin",
-    },
-    {
-        text:"''I like nonsense, it wakes up the brain cells. Fantasy is a necessary ingredient in living.''",
-        author:"-- Dr. Seuss",
-    },
-    {
-        text:"''That which does not kill us makes us stronger.''",
-        author:"-- Friedrich Nietzsche",
-    },
-    {
-        text:"''If you judge people, you have no time to love them.''",
-        author:"-- Mother Teresa",
-    },
-    {
-        text:"''It is never too late to be what you might have been''",
-        author:"-- George Eliot",
-    },
+
+
+// $('ul li a').click(function () {
+
+//     let id = $(this).attr('href');
+//     let offset = $(id).offset();
+//     $('body','html').animate({screenTop: offset}, 2000);
+
+// })
+
+
+
+$('.head').click(function() {
+    $(this).next('.body').slideToggle();
     
-];
+    $('.body').not($(this).next('.body')).slideUp(500);
+   
+  });
 
 
 
 
-  var quoteBtn =  document.getElementById('quoteBtn');
-  var quoteText =   document.getElementById('quoteText');
-  var quoteAuthor =   document.getElementById('quoteAuthor');
+  $('textarea').keyup(function () { 
+    
+
+let myLen = $(this).val().length;
+$('.count').text(
 
 
-var currentRandom;
-function getRandomNumber() {
-  var randomNumber = Math.floor(Math.random() * quotes.length);
+100 - myLen <= 0 ? 'your avali charcter finished' : 100 - myLen
 
-  while (randomNumber === currentRandom) {
-    randomNumber = Math.floor(Math.random() * quotes.length);
+
+)
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  window.onload = function() {
+   
+    countDownToTime("10 october 2023 9:56:00");
   }
-  currentRandom = randomNumber;
-  return randomNumber;
-}
 
-function getRandomQuotes() {
-    var randomQuotes = quotes[getRandomNumber()];
-    quoteText.innerHTML = randomQuotes.text;
-    quoteAuthor.innerHTML = randomQuotes.author;
+  function countDownToTime(countTo) {
+  
+        let futureDate = new Date(countTo);
+    futureDate = (futureDate.getTime()/1000);
+
+    let now = new Date();
+    now = (now.getTime()/1000);
+
+    timeDifference = (futureDate- now);
+        
+   let days = Math.floor( timeDifference / (24*60*60));
+   let hours = Math.floor((timeDifference - (days * (24*60*60))) / 3600);
+   let mins = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600 )) / 60);
+   let secs = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600) - (mins * 60)))
+
+
+    $(".days").html(`${days} D`);
+    $(".hours").html(`${hours} h`);
+    $(".minutes").html(`${ mins } m`);
+    $('.seconds').html(`${ secs} s`)
+
+  
+    setInterval(function() {  countDownToTime(countTo); }, 1000);
   }
-
-  quoteBtn.onclick = getRandomQuotes;
-
-
-
-
-
-
-
-
-
-// function myfunc(){
-
-//   var  emailInput = document.getElementById('emailInput');
-//   var  nameInput = document.getElementById('nameInput');
-//   var  domainInput = document.getElementById('domainInput');
-
-// var email = emailInput.value;
-
-// var atIndex = email.indexOf('@');
-
-
-
-// // if(atIndex != -1){
-// if(email.includes('@') == true){
-//     var name = emailInput.value.slice(0, atIndex);
-//     nameInput.value = name;
-//     domainInput.value = email.slice(atIndex+1);
-
-
-// }else{
-//     alert("plz write correct email");
-// }
-
-
-
